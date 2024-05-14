ba251ceeaf72940a54eeecad66aaf76a6c53d251
WonGi-Kim <kwg970310@gmail.com>
2024-05-14 17:08:06 +0900

    init commit

---
+++ src/main/java/com/sparta/springpersonalproject1/SpringPersonalProject1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springpersonalproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonalProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPersonalProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springpersonalproject1/SpringPersonalProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springpersonalproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
