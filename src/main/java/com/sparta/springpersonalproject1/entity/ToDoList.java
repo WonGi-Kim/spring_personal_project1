2d555c903c4aec369ace65f00f62187463003b7d
WonGi-Kim <kwg970310@gmail.com>
2024-05-17 16:44:07 +0900

    @valid 유효성 검사

--- src/main/java/com/sparta/springpersonalproject1/entity/ToDoList.java
+++ src/main/java/com/sparta/springpersonalproject1/entity/ToDoList.java
@@ -5,6 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,10 +20,21 @@ public class ToDoList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 200)
     private String title;
+
+    @NotBlank
+    @Size(max = 200)
     private String content;
+
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String manager;
+
+    @NotBlank
     private String password;
+
     private String date;
 
     public ToDoList(ToDoRequestDto toDoRequestDto) {
